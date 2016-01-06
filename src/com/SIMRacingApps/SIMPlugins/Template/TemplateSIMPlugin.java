9839b5ccc46212f812b36c7aa47f6e5cab1767b6
jfgilliam <jfgilliam@yahoo.com>
2016-01-06 13:50:26 -0500

    initial commit

---
+++ src/com/SIMRacingApps/SIMPlugins/Template/TemplateSIMPlugin.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.SIMRacingApps.SIMPlugins.Template;
+
+import com.SIMRacingApps.SIMPlugin;
+import com.SIMRacingApps.Server;
+
+/**
+ * @author Jeffrey Gilliam
+ * @copyright Copyright (C) 2015 - 2016 Jeffrey Gilliam
+ * @since 1.0
+ * @license Apache License 2.0
+ */
+public class TemplateSIMPlugin extends SIMPlugin {
+
+    /**
+     * @throws SIMPluginException
+     */
+    public TemplateSIMPlugin() throws SIMPluginException {
+        Server.logger().info("TemplateSIMPluging created");
+    }
+
+}
